5496774fa7f1d2c619cef441638d20014db636c7
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-24 13:10:53 -0600

    Avoid recursed layout calls as MeasureAction moves children.

--- XFSOAnswers/MyAbsoluteLayout.cs
+++ XFSOAnswers/MyAbsoluteLayout.cs
@@ -12,11 +12,27 @@ namespace XFSOAnswers
 		// Containing page will set this, to act on children after LayoutChildren.
 		public Action MeasureAction { get; set; }
 
+		private bool _busy;
+
 		protected override void LayoutChildren(double x, double y, double width, double height)
 		{
+			// Avoid recursed layout calls as MeasureAction moves children.
+			if (_busy)
+				return;
+
 			base.LayoutChildren(x, y, width, height);
 
-			MeasureAction?.Invoke();
+			_busy = true;
+			try
+			{
+				MeasureAction?.Invoke();
+			}
+			finally
+			{
+				_busy = false;
+				// Layout again, now that all children have been positioned.
+				base.LayoutChildren(x, y, width, height);
+			}
 		}
 	}
 }
