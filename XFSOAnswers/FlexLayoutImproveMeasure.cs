0b27010c6daf42a28228ba94a58d932bf8d5bcec
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-29 17:50:30 -0600

    Flexlayout Wrap - works

--- XFSOAnswers/FlexLayoutImproveMeasure.cs
+++ XFSOAnswers/FlexLayoutImproveMeasure.cs
@@ -5,18 +5,18 @@ namespace XFSOAnswers
 	class FlexLayoutImproveMeasure : FlexLayout
 	{
 
-		protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
-		{
-			var size = base.OnMeasure(double.PositiveInfinity, heightConstraint);
-			if (size.Request.Width >= widthConstraint - 1)
-			{
-				var size2 = base.OnMeasure(widthConstraint, heightConstraint);
-				size = size2;   // TODO
-				// TODO: What vertical spacing needed between lines?
-				//size = new SizeRequest(new Size(widthConstraint, 3 * size.Request.Height));
-			}
+		//protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+		//{
+		//	var size = base.OnMeasure(double.PositiveInfinity, heightConstraint);
+		//	if (size.Request.Width >= widthConstraint - 1)
+		//	{
+		//		var size2 = base.OnMeasure(widthConstraint, heightConstraint);
+		//		size = size2;   // TODO
+		//		// TODO: What vertical spacing needed between lines?
+		//		//size = new SizeRequest(new Size(widthConstraint, 3 * size.Request.Height));
+		//	}
 
-			return size;
-		}
+		//	return size;
+		//}
 	}
 }
--- XFSOAnswers/LabelImproveMeasure.cs
+++ XFSOAnswers/LabelImproveMeasure.cs
@@ -4,15 +4,15 @@ namespace XFSOAnswers
 {
 	class LabelImproveMeasure : Label
 	{
-		protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
-		{
-			var size = base.OnMeasure(widthConstraint, heightConstraint);
+		//protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+		//{
+		//	var size = base.OnMeasure(widthConstraint, heightConstraint);
 
-			//if (widthConstraint == double.PositiveInfinity)
-			//	// Force the minimum to always be the full size.
-			//	size = new SizeRequest(size.Request, size.Request);
+		//	//if (widthConstraint == double.PositiveInfinity)
+		//	//	// Force the minimum to always be the full size.
+		//	//	size = new SizeRequest(size.Request, size.Request);
 
-			return size;
-		}
+		//	return size;
+		//}
 	}
 }
