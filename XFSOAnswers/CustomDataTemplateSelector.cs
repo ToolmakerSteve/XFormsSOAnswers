5b97e9df751592b428a72a27c01c7c6607ca68d6
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-31 14:31:04 -0600

    DataTemplates : ResourceDictionary

---
+++ XFSOAnswers/CustomDataTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFSOAnswers
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CustomDataTemplateSelector : DataTemplateSelector
+	{
+		public BindableProperty FirstTemplateProperty = BindableProperty.Create(
+				nameof(FirstTemplate), typeof(DataTemplate), typeof(DataTemplates)
+			);
+		// TODO: Change to use Set/GetProperty of above BindableProperty.
+		public DataTemplate FirstTemplate { get; set; }
+
+		public BindableProperty SecondTemplateProperty = BindableProperty.Create(
+				nameof(SecondTemplate), typeof(DataTemplate), typeof(DataTemplates)
+			);
+		// TODO: Change to use Set/GetProperty of above BindableProperty.
+		public DataTemplate SecondTemplate { get; set; }
+
+
+		public CustomDataTemplateSelector()
+		{
+		}
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ XFSOAnswers/DataTemplates.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFSOAnswers
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DataTemplates : ResourceDictionary
+	{
+		public DataTemplates()
+		{
+		}
+	}
+}
