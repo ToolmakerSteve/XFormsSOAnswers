da603cde5c2ae71e2767cb367f78653af896a0f1
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2022-01-03 17:21:50 -0600

    ...

--- XFSOAnswers/OverlappingShapeButtonsPage.xaml.cs
+++ XFSOAnswers/OverlappingShapeButtonsPage.xaml.cs
@@ -12,11 +12,21 @@ namespace XFSOAnswers
 			InitializeComponent();
 		}
 
+		/// <summary>
+		/// This is called when any of the BoxViews are touched.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public void OnTapped1(object sender, EventArgs e)
 		{
 
 		}
 
+		/// <summary>
+		/// This is called when the grid is touched anywhere that is not inside one of the BoxViews.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public void OnTapped2(object sender, EventArgs e)
 		{
 
